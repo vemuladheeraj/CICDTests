ca0410d8a91e2cabc8d51a1efb15e14096dc797a
Dheeraj Vemula <52877699+vemuladheeraj@users.noreply.github.com>
2020-09-26 14:10:06 +0530

    test

--- CICDTest/Helpers/DriverOptionsSetEventArgs.cs
+++ CICDTest/Helpers/DriverOptionsSetEventArgs.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 namespace CICDTest.Helpers
 {
     public class DriverOptionsSetEventArgs : EventArgs
-    {
+    {//test chaes
+
         public DriverOptionsSetEventArgs(DriverOptions options)
         {
             this.DriverOptions = options;
